49432b44c2b2fa1677602cbc135f9bf55ba75117
Nurbol Bakasov <derg6626@gmail.com>
2026-03-12 10:30:08 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+func intToWord(param int) string {
+	word := [21]string{
+		"zero", "one", "two", "three", "four", "five",
+		"six", "seven", "eight", "nine", "ten",
+		"eleven", "twelve", "thirteen", "fourteen", "fifteen",
+		"sixteen", "seventeen", "eighteen", "nineteen", "twenty",
+	}
+	if param < 0 {
+		return "incorrect number"
+	}
+	if param > 20 {
+		return "big number"
+	}
+	return word[param]
+}
+
+func myCalculation(num1, num2 int, operation string) string {
+	result := 0
+	switch operation {
+	case "+":
+		result = num1 + num2
+	case "*":
+		result = num1 * num2
+	default:
+		panic("invalid operation")
+	}
+	return fmt.Sprintf("%s %s %s = %s",
+		intToWord(num1),
+		operation,
+		intToWord(num2),
+		intToWord(result))
+}
+func main() {
+	for i := 1; i <= 20; i++ {
+		multiplier := 3
+		fmt.Println(myCalculation(i, multiplier, "+"))
+		fmt.Println(myCalculation(i, multiplier, "*"))
+		fmt.Println("--------------------")
+	}
+}
